c58bbf8c08d3188b50421f30f102e3e3adfcc8c9
Andrey Vernov <a.vernov16@gmail.com>
2021-08-10 19:27:27 +0500

    step 4

---
+++ __tests__/filePaths.test.js
@@ -0,0 +1,14 @@
+import { test, expect } from '@jest/globals';
+import getData from '../src/filePaths.js';
+
+const data = {
+    "host": "hexlet.io",
+    "timeout": 50,
+    "proxy": "123.234.53.22",
+    "follow": false
+  };
+
+
+test('getData', () => {
+  expect(getData('./__fixtures__/file1.json')).toEqual(data)
+});
