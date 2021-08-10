202dc2b932b26fcb4a7814122c88a2901ac61e1c
Andrey Vernov <a.vernov16@gmail.com>
2021-08-10 16:11:09 +0500

    step 4

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,7 +2,7 @@ import { test, expect } from '@jest/globals';
 import getDiff from '../src/diffs.js';
 
 test('getDiff', () => {
-  expect(getDiff('file1.json', 'file2.json')).toEqual(`{
+  expect(getDiff('./__fixtures__/file1.json', './__fixtures__/file2.json')).toEqual(`{
   - follow: false
     host: hexlet.io
   - proxy: 123.234.53.22
