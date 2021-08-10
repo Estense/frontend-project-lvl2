7ed815f1e2aa36f15e79cbeb6fc217b4fbd3d9fd
Andrey Vernov <a.vernov16@gmail.com>
2021-08-10 20:17:15 +0500

    step 4

--- __tests__/filePaths.test.js
+++ __tests__/filePaths.test.js
@@ -2,13 +2,12 @@ import { test, expect } from '@jest/globals';
 import getData from '../src/filePaths.js';
 
 const data = {
-    "host": "hexlet.io",
-    "timeout": 50,
-    "proxy": "123.234.53.22",
-    "follow": false
-  };
-
+  host: 'hexlet.io',
+  timeout: 50,
+  proxy: '123.234.53.22',
+  follow: false,
+};
 
 test('getData', () => {
-  expect(getData('./__fixtures__/file1.json')).toEqual(data)
+  expect(getData('./__fixtures__/file1.json')).toEqual(data);
 });
