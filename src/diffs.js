521cd009df13d622b4cb33a17a59423bfae3c070
Andrey Vernov <a.vernov16@gmail.com>
2021-08-10 13:22:21 +0500

    step 3

--- src/diffs.js
+++ src/diffs.js
@@ -1,29 +1,26 @@
 import _ from 'lodash';
 import getData from './filePaths.js';
 
-
 const getDiff = (fileName1, fileName2) => {
   const obj1 = getData(fileName1);
   const obj2 = getData(fileName2);
   const a = _.union(Object.keys(obj1), Object.keys(obj2))
-             .sort()
-  const b = a.map((key) => { 
+    .sort();
+  const b = a.map((key) => {
     if (_.isEqual(obj1[key], obj2[key])) {
-      return `    ${key}: ${obj1[key]}`
+      return `    ${key}: ${obj1[key]}`;
     }
     if (!_.has(obj1, key) && _.has(obj2, key)) {
-      return `  + ${key}: ${obj2[key]}`
+      return `  + ${key}: ${obj2[key]}`;
     }
-
-    if (_.has(obj1,key) && !_.has(obj2, key)) {
-      return `  - ${key}: ${obj1[key]}`
+    if (_.has(obj1, key) && !_.has(obj2, key)) {
+      return `  - ${key}: ${obj1[key]}`;
     }
-    return `  - ${key}: ${obj1[key]}\n  + ${key}: ${obj2[key]}`
-  }
-  )
-  b.unshift('{')
-  b.push('}')
-  return b.join('\n')
+    return `  - ${key}: ${obj1[key]}\n  + ${key}: ${obj2[key]}`;
+  });
+  b.unshift('{');
+  b.push('}');
+  return b.join('\n');
 };
 
 export default getDiff;
--- src/filePaths.js
+++ src/filePaths.js
@@ -1,17 +1,12 @@
 import fs from 'fs';
 import path from 'path';
 
-
 const getPath = (fileName) => path.resolve(fileName);
-
 
 const getData = (fileName) => {
   const coding = 'utf8';
-  const path = getPath(fileName);
-  console.log(path);
-  return JSON.parse(fs.readFileSync(path, coding));
+  const filePath = getPath(fileName);
+  return JSON.parse(fs.readFileSync(filePath, coding));
 };
 
 export default getData;
-
-
